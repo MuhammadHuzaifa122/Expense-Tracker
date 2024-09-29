799b02d31fe45cc61eca636d515e770b0b9c9921
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-09-29 14:34:42 +0500

    Enable entering the data dynamically in my Expense Table

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@
 import './App.css'
 import ExpenseForm from './components/ExpenseForm'
 import ExpenseTable from './components/ExpenseTable'
+import ExpenseData from '../ExpenseData'
+import { useState } from 'react'
 
 function App() {
-
+const [expenses, setExpenses] = useState(ExpenseData)
   return (
     <main>
     <h1>Track Your Expense</h1>
     <div className="expense-tracker">
-      <ExpenseForm/>
-      <ExpenseTable/>
+      <ExpenseForm setExpenses={setExpenses}/>
+      <ExpenseTable expenses={expenses}/>
     </div>
   </main>
   )
