fb5c8d7243fefeada660c2338030ab4f3d310aed
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-01 16:11:53 +0500

    Add Sort functionality

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -28,9 +28,7 @@ export default function ExpenseTable({
       <table className="expense-table" onClick={() => {
         if (menuPosition.left) {
           setMenuPosition({})
-        }
-      }
-
+        }}
       }>
         <thead>
           <tr>
@@ -55,8 +53,7 @@ export default function ExpenseTable({
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
-                  onClick={(e) => {
-                    e.stopPropagation()
+                  onClick={() => {
                     setExpenses((prevState) => [...prevState.sort((a, b) => a.amount - b.amount)]);
                   }}
                 >
@@ -68,6 +65,9 @@ export default function ExpenseTable({
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
+                  onClick={() => {
+                    setExpenses((prevState) => [...prevState.sort((a, b) => b.amount - a.amount)]);
+                  }}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
