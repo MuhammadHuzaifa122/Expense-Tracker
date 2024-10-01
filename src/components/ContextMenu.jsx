c6167b4bf69dbc184c3a067e7cd62d7bb8238831
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-01 12:51:40 +0500

    Create a Context menu for Edit and Delete Options

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-export default function ContextMenu() {
+export default function ContextMenu({menuPosition, setMenuPosition}) {
+  if (!menuPosition.left) return
   return (
-    <div className="context-menu">
-      <div>Edit</div>
-      <div>Delete</div>
+    <div className="context-menu" style={menuPosition}>
+      <div onClick={() => {
+        console.log("Editing");
+        setMenuPosition({})
+      }}>Edit</div>
+      <div onClick={() => {
+        console.log("Deleting");
+        setMenuPosition({})
+      }}>Delete</div>
     </div>
   );
 }
